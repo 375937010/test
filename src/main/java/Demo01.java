392ce839d4887db75f69597a6919fab3772594f6
375937010 <375937010@qq.com>
2015-12-05 13:13:58 +0800

    Initial commit

---
+++ src/main/java/Demo01.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2015/12/3 0003.
+ */
+public class Demo01 {
+    public static void main(String[] avgs){
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+    }
+}
